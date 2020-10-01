192e561d0ce1bbbcb41c1c5cfce6c8c9dbff5563
jon deviney <developerdeviney1@gmail.com>
2020-09-30 19:02:08 -0600

    Clean up functions added

--- main.js
+++ main.js
@@ -1,48 +1,76 @@
 /* globals fetch moment */
 const url = 'http://localhost:3000/notes'
-
+const notesList = document.querySelector('#notes-list')
 
 document.addEventListener('submit', function (event) {
     event.preventDefault()
-    createNote()
+    createNotes()
+})
+
+notesList.addEventListener('click', function (event) {
+    if (event.target.matches('.delete')) {
+    deleteNotes(event.target.parentElement.dataset.id)
+    }
 })
 
 function showNotesList () {
 fetch(url)
-.then(res => res.json)
-.then(notesData => {
-    const notesList = document.querySelector('#notes-list')
-    for (const item of notesData) {
-    const notesItemEl = document.createElement('li')
-    notesItemEl.innerText = item.notesItem
-    notesList.appendChild(notesItemEl)
-    }
+    .then(res => res.json())
+    .then(notesData => {
+        for (const notes of notesData) {
+            showNotesItem(notes) 
+        }
     })  
 }
-function createNote () {
-    const notesInput = document.querySelector('#notes-input').value
-    console.log(notesInput)
 
-    fetch(url, {
+function showNotesItem (notes) {
+        const notesList = document.querySelector('#notes-list')
+        const notesItemEl = document.createElement('li')
+        notesItemEl.dataset.id = notes.id
+        notesItemEl.id = `item-${notes.id}`
+        notesItemEl.innerText = notes.notesItem
+        const deleteIcon = document.createElement('span')
+        deleteIcon.classList.add('fas', 'fa-times', 'mar-l-xs', 'delete')
+        notesItemEl.appendChild(deleteIcon)
+        notesList.appendChild(notesItemEl)
+}
+
+function createNotes() {
+    const notesInputField = document.querySelector('#notes-input')
+
+    const requestData = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify ({ notesItem: notesInput, created_at: moment().format() })
-})  
-    .then(res => res.json())
-    .then(data => {
-    const notesList = document.querySelector('#notes-list')
-    const notesItemEl = document.createElement('li')
-    notesItemEl.innerText = data.notesItem
-    notesList.appendChild(notesItemEl)
-    })  
-}
+        body: JSON.stringify ({ 
+            notesItem: notesInputField.value, 
+            created_at: moment().format() 
+        })
+    }
 
-showNotesList()
+        fetch(url, requestData) 
+            .then(res => res.json())
+            .then(data => {
+                notesInputField.value = ''
+                showNotesItem(data)
+        })
+} 
 
-function deleteNote (notesId) {
+function deleteNotes(notesId) {
     fetch(url + '/' + notesId, {
         method: 'DELETE'
     })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            const itemToRemove = document.querySelector(`#item-${notesId}`)
+            itemToRemove.remove()
+    })
 }
+
+showNotesList()
+
+// const notesList = document.querySelector('#notes-list')
+// const notesItemEl = document.createElement('li')
+// notesItemEl.innerText = data.notesItem
+// notesList.appendChild(notesItemEl)
+// })  
+// }
